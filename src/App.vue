a144ad9f684858a5cf67790256902be9f7a1e4c0
panuwat2114 <loverty29@gmail.com>
2020-08-21 17:34:37 +0700

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+ <div id="app">
+   
+    <HelloWorld msg="PANUWAT SRICHAIWIT" />
+    
+    <div class="row">
+      <div class="col-12"><img src="img/1.jpg" width="300" height="300" class="rounded-circle" />
+        <h1>{{total1}}</h1>
+      
+      <div class="col-12">
+        <CsButton @cc="sum" />
+      </div> <br> <br> </div>
+       <div class="col-12"><img src="img/2.jpg" width="300" height="300"  class="rounded-circle"/></div>
+     
+       <div class="col-12">
+
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-12">
+        <CsButton @cc="sum1" />
+      </div>
+    </div>
+    
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import CsButton from "./components/CSButton.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CsButton,
+    
+  },
+  data : function(){
+    return {
+    total1: 0,
+    total2: 0,
+   
+   } 
+  },
+  methods: {
+  sum(counterValue){
+    this.total1 = counterValue
+  },
+  
+  sum1(counterValue){
+    this.total2 = counterValue
+ 
+  
+},
+},
+
+};
+
 </script>
 
 <style>
@@ -22,7 +63,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color:black;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/CSButton.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div>
+    <button v-on:click="upFunction" class="btn btn-danger">ถูกใจ {{lable}}</button>
+    <button v-on:click="downFunction" class="btn btn-danger">ไม่ถูกใจ {{lable}}</button>
+    <button v-on:click="LoveFunction" class="btn btn-danger">รัก {{lable}}</button>
+    <button v-on:click="ResetFunction" class="btn btn-danger">รีเซ็ท {{lable}}</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+    downFunction: function () {
+      this.counter += -5;
+      this.$emit("cc", this.counter);
+    },
+
+    LoveFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+<style>
+</style>
